0462a2d980c7dc2133ba55f782a308f097d22656
Hrishi9911 <hrishikesh@unifyxperts.com>
2024-11-26 06:21:21 +0000

    first commit

---
+++ test_app/testing_module/doctype/registration_form/test_registration_form.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Hrishikesh and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRegistrationForm(FrappeTestCase):
+	pass
